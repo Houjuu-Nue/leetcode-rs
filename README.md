7ad46d0aa5f54f4641c1e5a69844a268cd16ddfe
Houjuu-Nue <usami-ssc@protonmail.com>
2019-04-09 22:52:27 +0800

    :bookmark: Add Solution to #P4.

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ $ cargo test -- t1 # run test case for problem #1
 
 | #                     | Title                                                        | Difficulty | Best Submit |
 | --------------------- | ------------------------------------------------------------ | ---------- | ----------- |
+| [#4](src/p00xx/p4.rs) | [Median of Two Sorted Arrays](https://leetcode.com/problems/median-of-two-sorted-arrays/) | Hard       | 8ms, 2.5MB  |
 | [#3](src/p00xx/p3.rs) | [Longest Substring Without Repeating Characters](https://leetcode.com/problems/longest-substring-without-repeating-characters/) | Medium     | 4ms, 2.5MB  |
 | [#2](src/p00xx/p2.rs) | [Add Two Numbers](https://leetcode.com/problems/add-two-numbers/) | Medium     | 8ms, 2.3MB  |
 | [#1](src/p00xx/p1.rs) | [Two Sum](https://leetcode.com/problems/two-sum/)            | Easy       | 4ms, 3.3MB  |
--- src/p00xx/p4.rs
+++ src/p00xx/p4.rs
@@ -28,20 +28,85 @@
 
 #[derive(Debug, Clone)]
 pub struct Input {
-    pub num1: Vec<i32>,
-    pub num2: Vec<i32>
+    pub nums1: Vec<i32>,
+    pub nums2: Vec<i32>
 }
 
 pub type Answer = f64;
 
 pub trait Solution {
-    fn find_median_sorted_arrays(&self, num1: Vec<i32>, num2: Vec<i32>) -> f64;
+    fn find_median_sorted_arrays(&self, nums1: Vec<i32>, nums2: Vec<i32>) -> f64;
 }
 
+// Approach 1: Brute Force
 pub struct Solution1;
 impl Solution for Solution1 {
 
-    fn find_median_sorted_arrays(&self, num1: Vec<i32>, num2: Vec<i32>) -> f64 {
-        2.0
+    fn find_median_sorted_arrays(&self, nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
+
+        enum SampleNum { N1, N2 }
+        
+        let len1 = nums1.len();
+        let len2 = nums2.len();
+
+        let mut flag1 = (len1 + len2) % 2 != 0;
+        let flag2 = flag1;
+        let middle = (len1 + len2 - 1) / 2;
+
+        let mut i  = 0;
+        let mut p1 = 0;
+        let mut p2 = 0;
+        let mut sum: f64 = 0.0;
+
+        'MARK: loop {
+
+            // Decide sample number from nums1 or nums2.
+            let path = match (p1, p2) {
+                | (l1, _) if l1 == len1             => SampleNum::N2,
+                | (_, l2) if l2 == len2             => SampleNum::N1,
+                | (p1, p2) if nums1[p1] < nums2[p2] => SampleNum::N1,
+                | _                                 => SampleNum::N2,
+            };
+            
+            match path {
+                | SampleNum::N1 => {
+                    
+                    if i >= middle {
+                        sum += nums1[p1] as f64;
+
+                        if flag1 {
+                            break 'MARK
+                        } else {
+                            flag1 = true;
+                        }
+                    }
+
+                    p1 += 1;
+                },
+                | SampleNum::N2 => {
+                    
+                    if i >= middle {
+                        sum += nums2[p2] as f64;
+
+                        if flag1 {
+                            break 'MARK
+                        } else {
+                            flag1 = true;
+                        }
+                    }
+
+                    p2 += 1;
+                },
+            }
+
+            i += 1;
+        }
+
+        // calculate the median
+        if flag2 {
+            sum
+        } else {
+            sum / 2.0
+        }
     }
 }
