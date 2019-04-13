41de4237fcfc535dd5d01e4e60f980007793a66b
Houjuu-Nue <usami-ssc@protonmail.com>
2019-04-13 22:48:52 +0800

    :bookmark: Implment Expand Around Center solution for #P5.

--- src/p00xx/p5.rs
+++ src/p00xx/p5.rs
@@ -26,7 +26,7 @@ pub trait Solution {
     fn longest_palindrome(&self, s: String) -> String;
 }
 
-// Approach 0: Dynamic Programming ---------------------------------------------
+// Approach 0: Dynamic Programming Version 1 ----------------------------------
 pub struct Solution0;
 impl Solution for Solution0 {
 
@@ -34,29 +34,27 @@ impl Solution for Solution0 {
         
         let bytes = s.into_bytes();
         let len = bytes.len();
-        if len <= 1 { return String::from_utf8(bytes).unwrap() }
+        if len <= 1 { return unsafe { String::from_utf8_unchecked(bytes) } }
 
         // p store the result.
         let mut p: Vec<Vec<bool>> = vec![vec![false; len]; len];
-        
-        // Initialize boundary.
-        for i in 0..len {
-            p[i][i] = true;
-        }
-        for i in 0..(len - 1) {
-            p[i][i + 1] = bytes[i] == bytes[i + 1];
-        }
 
-        // Start to search
         let mut max_length = 1;
         let mut max_start  = 0;
 
+        // Start to search
         for j in 1..len {
+
+            // Initialize boundary.
+            p[j][j] = true;
+
             for i in 0..j {
 
-                if j - i != 1 {
-                    p[i][j] = p[i + 1][j - 1] && bytes[i] == bytes[j];
-                }
+                p[i][j] = if j - i != 1 {
+                    p[i + 1][j - 1] && bytes[i] == bytes[j]
+                } else {
+                    bytes[i] == bytes[j]
+                };
 
                 if p[i][j] == true {
                     let length = j - i + 1;
@@ -76,13 +74,51 @@ impl Solution for Solution0 {
 
     }
 }
+// -----------------------------------------------------------------------------
 
+// Approach 1: Dynamic Programming Version 2 -----------------------------------
+pub struct Solution1;
+impl Solution for Solution1 {
+
+    fn longest_palindrome(&self, s: String) -> String {
+
+        let bytes = s.into_bytes();
+        let len = bytes.len();
+        if len <= 1 { return unsafe { String::from_utf8_unchecked(bytes) } }
+
+        let mut dp: Vec<Vec<bool>> = vec![vec![false; len]; len];
+        let mut left  = 0;
+        let mut right = 0;
+
+        let mut line = len - 1;
+        while let Some(i) = line.checked_sub(1) {
+            dp[i][i] = true;
+
+            for j in (i + 1)..len {
+                dp[i][j] = bytes[i] == bytes[j] && (j - i < 3 || dp[i + 1][j - 1]);
+
+                if dp[i][j] && right - left <= j - i {
+                    left  = i;
+                    right = j;
+                }
+            }
+
+            line = i;
+        }
+
+        unsafe {
+            let sub_bytes = bytes.get_unchecked(left..=right)
+                .iter().cloned().collect();
+            String::from_utf8_unchecked(sub_bytes)
+        }
+    }
+}
 // -----------------------------------------------------------------------------
 
 
-// Approach 1: Brute Force -----------------------------------------------------
-pub struct Solution1;
-impl Solution for Solution1 {
+// Approach 2: Brute Force -----------------------------------------------------
+pub struct Solution2;
+impl Solution for Solution2 {
 
     fn longest_palindrome(&self, s: String) -> String {
 
@@ -128,5 +164,80 @@ fn is_palindrome(s: &[u8], mut left: usize, mut right: usize) -> bool {
     }
 
     true
+}
+// -----------------------------------------------------------------------------
+
+// -----------------------------------------------------------------------------
+// Approach 3: Expand Around Center
+pub struct Solution3;
+impl Solution for Solution3 {
+
+    fn longest_palindrome(&self, s: String) -> String {
+
+        let bytes = s.into_bytes();
+        if bytes.len() <= 1 { return String::from_utf8(bytes).unwrap() }
+
+        let mut max_range = MaxRange { start: 0, length: 0 };
+
+        for i in 0..bytes.len() {
+
+            let max_odd  = expend_odd(&bytes, i);
+            let max_even = expend_even(&bytes, i);
+
+            let max_tmp = if max_odd.length > max_even.length { max_odd } else { max_even };
+
+            if max_tmp.length > max_range.length {
+                max_range = max_tmp;
+            }
+        }
+
+        let sub_bytes = bytes.get(max_range.start..(max_range.start + max_range.length))
+            .unwrap().iter().cloned().collect();
+        String::from_utf8(sub_bytes).unwrap()
+    }
+}
+
+struct MaxRange {
+    start : usize,
+    length: usize,
+}
+
+fn expend_odd(s: &[u8], center: usize) -> MaxRange {
+
+    let len = s.len() as i32;
+    let mut left  = (center as i32) - 1;
+    let mut right = center as i32 + 1;
+    let mut length = 1;
+    let mut start = center;
+
+    while left >= 0 && right < len && s[left as usize] == s[right as usize] {
+        length += 2;
+        start -= 1;
+
+        left  -= 1;
+        right += 1;
+    }
+
+    MaxRange { start, length }
+}
+
+fn expend_even(s: &[u8], left: usize) -> MaxRange {
+
+    let mut length = 0;
+    let mut start = left + 1;
+
+    let len = s.len() as i32;
+    let mut left  = left as i32;
+    let mut right = left + 1;
+
+    while left >= 0 && right < len && s[left as usize] == s[right as usize] {
+        length += 2;
+        start -= 1;
+
+        left  -= 1;
+        right += 1;
+    }
+
+    MaxRange { start, length }
 }
 // -----------------------------------------------------------------------------
