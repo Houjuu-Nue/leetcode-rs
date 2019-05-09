c1e8ddb777db4937fb8160341437695e7de6062b
unknownue <usami-ssc@protonmail.com>
2019-05-09 22:40:51 +0800

    :umbrella: Small fix to template generation.

--- template/doc.rs
+++ template/doc.rs
@@ -132,7 +132,9 @@ fn translate_doc(mut file: File, path: String) -> std::io::Result<File> {
                     }
                     
                     translation.push_str(sentence);
-                    translation.push('.');
+                    if sentence.ends_with(':') == false {
+                        translation.push('.');
+                    }
                     translation.push_str("\n");
                     translation.push_str(LINE_PREFIX_V2);
                     translation.push_str("\n");
