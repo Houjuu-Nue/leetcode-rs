f938413c7e068d4b0823e553dcf8dbaf29f567d9
unknownue <usami-ssc@protonmail.com>
2019-06-23 18:35:55 +0800

    :bookmark: Generalize hash based methods for #P49.

--- src/p00xx/p49.rs
+++ src/p00xx/p49.rs
@@ -33,26 +33,65 @@ pub trait Solution {
 }
 
 // -----------------------------------------------------------------------------
-/// Approach 0: Brute Force.
+/// Approach 0: Hash with sorted numbers.
 pub struct Solution0;
 impl Solution for Solution0 {
 
     fn group_anagrams(&self, strs: Vec<String>) -> Vec<Vec<String>> {
-
-        use std::collections::HashMap;
-
-        let mut result: HashMap<Vec<u8>, Vec<String>> = HashMap::new();
-        for s in strs {
-            let mut str_sorted = s.clone().into_bytes();
-            str_sorted.sort_unstable();
-
-            result.entry(str_sorted).or_insert(Vec::new())
-                .push(s);
-        }
-
-        result.into_iter()
-            .map(|(_, strs)| strs).collect()
+        hash_for_group_anagrams(strs, hash_with_sorted_numbers)
     }
+}
+
+fn hash_for_group_anagrams<Key, HashFunc>(strs: Vec<String>, hash_key: HashFunc) -> Vec<Vec<String>>
+    where
+        Key: std::hash::Hash + Eq,
+        HashFunc: Fn(&String) -> Key {
+
+    use std::collections::HashMap;
+
+    let mut result: HashMap<Key, Vec<String>> = HashMap::new();
+    for s in strs {
+        let key = hash_key(&s);
+        result.entry(key).or_insert(Vec::new())
+            .push(s);
+    }
+
+    result.into_iter()
+        .map(|(_, strs)| strs).collect()
+}
+
+fn hash_with_sorted_numbers(key: &String) -> Vec<u8> {
+
+    let mut str_sorted = key.clone().into_bytes();
+    str_sorted.sort_unstable();
+    str_sorted
+}
+// -----------------------------------------------------------------------------
+
+
+// -----------------------------------------------------------------------------
+/// Approach 1: Hash with prime numbers multiplication.
+pub struct Solution1;
+
+// Each prime number represent a letter.
+const PRIME_NUMBERS: [u64; 26] = [
+    2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+    31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
+    73, 79, 83, 89, 97, 101,
+];
+
+impl Solution for Solution1 {
+
+    fn group_anagrams(&self, strs: Vec<String>) -> Vec<Vec<String>> {
+        hash_for_group_anagrams(strs, hash_with_primes)
+    }
+}
+
+fn hash_with_primes(key: &String) -> u64 {
+    key.chars().map(|ch| {
+        let index = (ch as u8 - 'a' as u8) as usize;
+        PRIME_NUMBERS[index]
+    }).product()
 }
 // -----------------------------------------------------------------------------
 
